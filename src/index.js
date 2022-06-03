bdda33fb31cf0c1786e8ed5d09d8d87ac8e9a382
Kate Yankovenko <k.yankovenko@ukr.net>
2022-06-03 18:05:06 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@
 // import './css/styles.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
