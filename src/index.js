c01c75154255a338c0c782442c526401f0c636d8
Kate Yankovenko <k.yankovenko@ukr.net>
2022-06-03 17:56:38 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // error_reporting(E_ERROR | E_PARSE);
-import './css/styles.css';
+// import './css/styles.css';
 import Notiflix from 'notiflix';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
