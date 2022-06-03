fa0dccd66caf1048ae575b9ad9624785e261453c
Kate Yankovenko <k.yankovenko@ukr.net>
2022-06-03 18:07:19 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,3 +5,18 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchPixaApiService from './pixabay';
+
+
+const refs = {
+    searchForm: document.querySelector('.search-form'),
+    gallery: document.querySelector('.gallery'),
+    loadBtn: document.querySelector('.load-more'),
+}
+const ApiService = new fetchPixaApiService();
+const lightbox = new SimpleLightbox('.photo-link', {
+  overlayOpacity: 0.4,
+  animationSpeed: 100,
+});
+
+refs.searchForm.addEventListener('submit', onSearchForm);
+refs.loadBtn.addEventListener('click', onLoadMoreBtn);
