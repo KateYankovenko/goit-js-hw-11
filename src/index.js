334ddaab5193efbe55e3a4e1b5e37ca549766bbf
Kate Yankovenko <k.yankovenko@ukr.net>
2022-06-06 21:57:18 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import fetchPixaApiService from './pixabay';
+import { renderCard } from './renderHTML';
 
 
 const refs = {
     searchForm: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
-    loadBtn: document.querySelector('.load-more'),
+    loadMoreBtn: document.querySelector('.load-more'),
 }
 const ApiService = new fetchPixaApiService();
 const lightbox = new SimpleLightbox('.photo-link', {
@@ -19,7 +20,7 @@ const lightbox = new SimpleLightbox('.photo-link', {
 });
 
 refs.searchForm.addEventListener('submit', onSearchForm);
-refs.loadBtn.addEventListener('click', onLoadMoreBtn);
+refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
 
 
 async function onSearchForm(e) {
@@ -51,6 +52,7 @@ async function onSearchForm(e) {
       'Sorry, there are no images matching your search query. Please try again.',
     );
   }
+  chekSearchEnd();
 // "We're sorry, but you've reached the end of search results."
 } 
 
@@ -85,9 +87,9 @@ function resetMarkup() {
   refs.gallery.innerHTML = '';
 }
 
-// function rewrightMarkup(markup) {
-//   refs.gallery.insertAdjacentHTML('beforeend', imgTemplate(markup));
-// }
+function rewrightMarkup(markup) {
+  refs.gallery.insertAdjacentHTML('beforeend', renderCard(markup));
+}
 
 function chekIfInpIsEmpty() {
   Notiflix.failure('Please, enter something in search field!');
@@ -96,6 +98,10 @@ function chekIfInpIsEmpty() {
   resetMarkup();
 }
 
+function chekSearchEnd() {
+  hideShowMoreBtn();
+  Notify.info("We're sorry, but you've reached the end of search results");
+}
 
 // Зробити плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень.
 function smoothScroll(){
--- src/renderHTML.js
+++ src/renderHTML.js
@@ -2,6 +2,54 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryEl = document.querySelector('.gallery');
-const galleryLigthbox = new SimpleLightbox('.gallery a');
+
+export function renderCard() {
+  const markup = `<a href='{{largeImageURL}}' class='photo-link'>
+// <div class="photo-card">
+//   <img src="{{webformatURL}}" alt="{{tags}}" loading="lazy"/></a>
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes {{likes}}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views {{views}}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments {{comments}}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads {{downloads}}</b>
+//     </p>
+//   </div>
+// </div>
+// </a>`;
+  
+    galleryEl.insertAdjacentHTML('beforeend', markup);
+}
 
 
+// {{#each data.hits}}
+// <a href='{{largeImageURL}}' class='photo-link'>
+// <div class="photo-card">
+//   <img src="{{webformatURL}}" alt="{{tags}}" loading="lazy"/></a>
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes {{likes}}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views {{views}}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments {{comments}}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads {{downloads}}</b>
+//     </p>
+//   </div>
+// </div>
+// </a>
+// {{/each}}
+
+
+//   {{!-- <a href='' class='photo-link'>
+//   <img src='' alt='' loading='lazy' /> --}}
