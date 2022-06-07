e23bdb073cfb27363ff364ee6a1fd71b50b0b568
Kate Yankovenko <k.yankovenko@ukr.net>
2022-06-07 21:44:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,103 @@
 // // error_reporting(E_ERROR | E_PARSE);
 // import './css/styles.css';
-// import Notiflix from 'notiflix';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
-// import SimpleLightbox from 'simplelightbox';
-// import 'simplelightbox/dist/simple-lightbox.min.css';
-// import fetchPixaApiService from './pixabay';
-// import { renderCard } from './renderHTML';
+
+import NewApiService from './pixabay';
+import { hitsPhotoCard } from './cardsRender';
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import throttle from 'lodash.throttle';
+
+const newApiService = new NewApiService();
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('[data-action="load-more"]'), 
+}
+//some variable for noti messages
+let cardsLeft;
+
+refs.loadMoreBtn.classList.add('is-hidden');
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  clearCardSet();
+  smoothScroll();
+  refs.loadMoreBtn.classList.add('is-hidden');
+  newApiService.resetPage();
+  newApiService.query = e.currentTarget.elements.searchQuery.value;
 
 
+  if (newApiService.query === '') {
+    Notify.failure('Please fill in the field');
+    return;
+  }
+  newApiService
+    .fetchData()
+    .then(({ totalHits, hits }) => {
+      //hits are in the array of hits
+      if (hits.length === 0) {
+        Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again',
+        );
+        return;
+      }
+      Notify.success(`Hooray! We found ${totalHits} images!`);
+      hitsPhotoCard(hits);
 
-// const refs = {
-//     searchForm: document.querySelector('.search-form'),
-//     gallery: document.querySelector('.gallery'),
-//     loadMoreBtn: document.querySelector('.load-more'),
+      refs.loadMoreBtn.classList.remove('is-hidden');
+
+      if (cardsLeft === 1) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+        return;
+      }
+    })
+    .catch(error=>console.log(error));
+    // .catch(error => { console.log(Notify.failure(
+    //     'Sorry, there are no images matching your search query. Please try again.',
+    //   )) });
+}
+
+
+function onLoadMore() {
+  smoothScroll();
+  newApiService.fetchData().then(({ hits }) => {
+    if (newApiService.query === cardsLeft) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      return;
+    }
+    hitsPhotoCard(hits);
+  });
+}
+
+function clearCardSet() {
+  refs.gallery.innerHTML = '';
+}
+
+// function chekIfInpIsEmpty() {
+//   Notify.failure('Please, enter something in search field!');
+
+//   loadMoreBtn.classList.add('is-hidden');
+//   clearCardSet();
 // }
-// const ApiService = new fetchPixaApiService();
-// const lightbox = new SimpleLightbox('.photo-link', {
-//   overlayOpacity: 0.4,
-//   animationSpeed: 100,
-// });
 
-// refs.searchForm.addEventListener('submit', onSearchForm);
-// refs.loadMoreBtn.addEventListener('click', onLoadMoreBtn);
+// Зробити плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень.
+function smoothScroll(){
+    const { height: cardHeight } = document
+    .querySelector(".gallery")
+    // .firstElementChild.getBoundingClientRect();
+
+    window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+    });
+}
 
 
 // async function onSearchForm(e) {
@@ -88,134 +164,19 @@
 //   refs.loadMoreBtn.classList.add('is-hidden');
 // }
 
-// function resetMarkup() {
-//   refs.gallery.innerHTML = '';
-// }
 
 // function rewrightMarkup(markup) {
 //   refs.gallery.insertAdjacentHTML('beforeend', renderCard(markup));
 // }
 
-// function chekIfInpIsEmpty() {
-//   Notiflix.failure('Please, enter something in search field!');
-
-//   notShowLoadBtn();
-//   resetMarkup();
-// }
 
 // function chekSearchEnd() {
 //   notShowLoadBtn();
 //   Notify.info("We're sorry, but you've reached the end of search results");
 // }
 
-// // Зробити плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень.
-// function smoothScroll(){
-//     const { height: cardHeight } = document
-//     .querySelector(".gallery")
-//     .firstElementChild.getBoundingClientRect();
-
-//     window.scrollBy({
-//     top: cardHeight * 2,
-//     behavior: "smooth",
-//     });
-// }
-
-// import './css/styles.css';
-import NewApiService from './pixabay';
-import { hitsPhotoCard } from './cardsRender';
-import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import throttle from 'lodash.throttle';
-
-const newApiService = new NewApiService();
-
-const form = document.querySelector('#search-form');
-const gallery = document.querySelector('.gallery');
-const loadMoreBtn = document.querySelector('[data-action="load-more"]');
 
 
-form.addEventListener('submit', onFormSubmit);
-loadMoreBtn.addEventListener('click', onLoadMore);
-loadMoreBtn.classList.add('is-hidden');
-
-function onFormSubmit(e) {
-  e.preventDefault();
-  clearCardSet();
- 
-  loadMoreBtn.classList.add('is-hidden');
-  newApiService.resetPage();
-  newApiService.query = e.currentTarget.elements.searchQuery.value;
-
-  if (newApiService.query.trim() === '') {
-    chekIfInpIsEmpty();
-    return;
-  }
-
-  smoothScroll();
-
-  newApiService
-    .fetchData()
-    .then(({ totalHits, hits }) => {
-      Notify.success(`Hooray! We found ${totalHits} images!`);
-      hitsPhotoCard(hits);
-      
-      loadMoreBtn.classList.remove('is-hidden');
-
-      // if (newApiService === 1) {
-      //   loadMoreBtn.classList.add('is-hidden');
-      //   Notify.info(
-      //     "We're sorry, but you've reached the end of search results."
-      //   );
-      //   return;
-      // }
-    })
-      .catch(error =>
-       Notify.failure(
-        'Sorry, there are no images matching your search query. Please try again.',
-      )); 
-}
-
-
-function onLoadMore() {
-  // e.preventDefault();
-  smoothScroll();
-  newApiService.fetchData().then(({ hits }) => {
-    if (newApiService.nowPage() <= 40) {
-      
-      loadMoreBtn.classList.add('is-hidden');
-      Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-        return;
-    }
-    hitsPhotoCard(hits);
-  });
-}
-
-function clearCardSet() {
-  gallery.innerHTML = '';
-}
-
-
-function chekIfInpIsEmpty() {
-  Notify.failure('Please, enter something in search field!');
-
-  loadMoreBtn.classList.add('is-hidden');
-  clearCardSet();
-}
-
-
-// Зробити плавне прокручування сторінки після запиту і відтворення кожної наступної групи зображень.
-function smoothScroll(){
-    const { height: cardHeight } = document
-    .querySelector(".gallery")
-    // .firstElementChild.getBoundingClientRect();
-
-    window.scrollBy({
-    top: cardHeight * 2,
-    behavior: "smooth",
-    });
-}
 
 
 
